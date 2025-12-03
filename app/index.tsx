e4ef9175cd99f36d76768cfc08d591760ab3b887
Nalin-Sandaruwan <creationbynalin@gmail.com>
2025-12-03 12:03:01 +0530

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View 
+      // style={{
+      //   flex: 1,
+      //   justifyContent: "center",
+      //   alignItems: "center",
+      // }}
+    >
+      <Text className="flex bg-black text-white text-5xl font-bold w-full text-center">Welcome to world</Text>
+    </View>
+  );
+}
