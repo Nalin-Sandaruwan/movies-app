22c7371c68b4f03b7e2088f90d693abef176ba6f
Nalin-Sandaruwan <creationbynalin@gmail.com>
2026-01-09 16:53:30 +0530

    Movie app react native initialized

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      className="flex-1 items-center justify-center bg-white px-4"
+    >
+      <Text className="flex text-gray-900 text-5xl font-bold w-full text-center">Welcome to world</Text>
+      <Text className="text-center">you can going to on boarding that </Text>
+      
+      {/* <StatusBar style="auto" /> */}
+    </View>
+  );
+}
